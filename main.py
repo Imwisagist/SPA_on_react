31dd8ca814b2702dcb2bf316d0391c4dc52ae52a
Дмитрий Спиряев <imwisagist@yandex.ru>
2023-04-05 20:15:30 +0300

    v0.1 /articles/{art_id} endpoint added.

---
+++ main.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+
+from aiohttp import ClientSession, ClientResponse
+from fastapi import FastAPI
+
+app = FastAPI()
+ENDPOINT = 'https://api.forum-auto.ru/v2/listGoods'
+
+
+@app.get("/articles/{art_id}")
+async def get_list_goods(art_id):
+    params = {'login': '493358_stroyzar', 'pass': 'sAVDkrEbqd', 'art': art_id}
+    async with ClientSession() as session:
+        response: ClientResponse = await session.get(ENDPOINT, params=params)
+
+        if response.status != HTTPStatus.OK:
+            return {
+                'result': 'Remote API not available. Status code - {0}'.format(
+                    response.status
+                )
+            }
+        response_json: dict = await response.json()
+        if 'errors' in response_json:
+            return response_json.get('errors').get('FaultString')
+
+        return response_json
